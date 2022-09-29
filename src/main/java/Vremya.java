4e6343ccb6f827a08379b5339ebbe50004bebc10
DanilaCrazych <Mag7istr@mail.ru>
2022-09-29 23:13:15 +0300

    Кокунин так и не сдал

--- src/main/java/Vremya.java
+++ src/main/java/Vremya.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Vremya {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Vremya
+{
+    public static void Clocks()
+
+    {
+        String input = "Кокунин не сдал экзамен по вождению в: 11:55";
+        Pattern pattern = Pattern.compile("(2[0-3]|[0-1]\\d):[0-5]\\d");
+        Matcher matcher = pattern.matcher(input);
+        while(matcher.find())
+        {
+            System.out.println(matcher.group());
+        }
+    }
+
 }
